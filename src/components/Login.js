261baeb06f469ce064e86441a30789eeee37d9c3
everpe <ever.penab@autonoma.edu.co>
2019-12-11 21:21:57 -0500

    Componente Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,25 +17,16 @@ class Login extends Component
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div className="form-group">
-                 <label for="exampleInputPassword1">Password</label>
-                 <input type="password" class="form-control" id="exampleInputPassword1"> 
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1"/> 
             </div>
-
+            <div className="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
         
-           
-       
-      
-           
-             
-
-            {/* {
-            <div className="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label className="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button> } */}
-        {/* <form/> */}
 
       );
     }
